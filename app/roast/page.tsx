abd3516b5981e0f5a62ccefc4ff98952841b641d
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2024-12-02 21:02:37 +0800

    Make the list more beautiful

--- app/roast/page.tsx
+++ app/roast/page.tsx
@@ -27,19 +27,28 @@ const MainRoast = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center text-white">
       <div className="text-center">
-        <h1 className="text-3xl mb-4">😈 Roast someone 😈</h1>
+        <h1 className="text-3xl">😈 Roast someone 😈</h1>
+        <p className="mb-4">
+          You can copy a shareable link to the ones you want to share or view it
+        </p>
         {/* list of links */}
-        <div className="grid grid-cols-1 bg-white text-black p-2">
+        <div className="grid grid-cols-1  text-black p-2">
           {roastData.roasts.map((e, idx) => {
             return (
-              <div key={idx} className="grid grid-cols-2">
+              <div
+                key={idx}
+                className="bg-white py-1 px-4 my-1 rounded-md grid grid-cols-2"
+              >
                 <div className="col-span-1 text-left">{e.name}</div>
                 <div className="col-span-1 flex justify-end ">
                   <Copy
-                    className="pr-1"
+                    className="pr-1 hover:text-purple-500 hover:cursor-pointer"
                     onClick={() => handleCopyLink(e.name)}
                   />
-                  <Eye onClick={() => handleViewLink(e.name)} />
+                  <Eye
+                    className="hover:text-purple-500 hover:cursor-pointer"
+                    onClick={() => handleViewLink(e.name)}
+                  />
                 </div>
               </div>
             );
